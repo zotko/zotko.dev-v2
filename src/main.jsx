7f8d4717b1d8ce944e0fc3b05de7aa9e54be3680
Mykola Zotko <mzotko@gmail.com>
2025-10-15 21:58:42 +0200

    refactor: update meta description and author title; remove chat widget and related components

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,1 @@
 import './style.css'
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import ChatWidget from './App.jsx'
-
-ReactDOM.createRoot(document.getElementById('chat-widget')).render(
-  <React.StrictMode>
-    <ChatWidget />
-  </React.StrictMode>,
-)
